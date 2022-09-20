8ff260b4107feff19a3c2c7a61248d9d757d838e
valberjr <3cifhzht@duck.com>
2022-09-20 17:09:01 -0300

    api proxy config

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { Course } from '../model/course';
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) {}
 
